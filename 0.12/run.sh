fce76745ed314ee085426bf9734a34a46044e9d4
Maurits van Mastrigt <maurits@vanmastrigt.nl>
2016-05-09 15:52:14 +0200

    Escape username and database in run script (#82).

--- 0.12/run.sh
+++ 0.12/run.sh
@@ -118,12 +118,12 @@ if [ -n "${PRE_CREATE_DB}" ]; then
         PASS=${INFLUXDB_INIT_PWD:-root}
         if [ -n "${ADMIN_USER}" ]; then
           echo "=> Creating admin user"
-          influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="CREATE USER ${ADMIN_USER} WITH PASSWORD '${PASS}' WITH ALL PRIVILEGES"
+          influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="CREATE USER \"${ADMIN_USER}\" WITH PASSWORD '${PASS}' WITH ALL PRIVILEGES"
           for x in $arr
           do
               echo "=> Creating database: ${x}"
-              influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="create database ${x}"
-              influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="grant all PRIVILEGES on ${x} to ${ADMIN_USER}"
+              influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="create database \"${x}\""
+              influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="grant all PRIVILEGES on \"${x}\" to ${ADMIN_USER}"
           done
           echo ""
         else
